4ad8ea4f1653ef03f0c5bc86be9d6bd8e3f925fa
wardou2 <ward92@gmail.com>
2021-04-06 13:33:39 -0700

    Add router and set up basic framework for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./components/Login";
+import PatientRecords from "./components/PatientRecords";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState<string>();
+
+    if (!token) {
+        return <Login />;
+    }
+
+    return (
+        <div>
+            <h1>RIBS</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/records">
+                        <PatientRecords />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PatientRecords.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PatientRecords = () => {
+    return <h1>Patient Records</h1>;
+};
+
+export default PatientRecords;
