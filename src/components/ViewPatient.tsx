0482cdc379d7aa2464ba3bcee7399307a3cc3f63
wardou2 <ward92@gmail.com>
2021-04-13 21:08:27 -0700

    Improve patient view page

--- src/components/ViewPatient.tsx
+++ src/components/ViewPatient.tsx
@@ -1,30 +1,65 @@
 import React, { useEffect, useState } from "react";
-
 import { useParams } from "react-router-dom";
+import { Table } from "semantic-ui-react";
 
-import { Patient } from "../interfaces";
+import { Patient, ParamTypes } from "../interfaces";
 import { getPatient } from "../api/Patients";
-
-interface ParamTypes {
-    patientId?: string;
-}
+import { utcToAge } from "../util/Util";
 
 const ViewPatient = () => {
     const { patientId } = useParams<ParamTypes>();
     const [patient, setPatient] = useState<Patient>();
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        getPatient(Number(patientId)).then(setPatient);
+        const retrievePatient = async () => {
+            try {
+                const patient = await getPatient(Number(patientId));
+                setPatient(patient);
+            } catch (e) {
+                setError(e.message);
+            }
+        };
+        retrievePatient();
     }, [patientId]);
 
+    if (error || !patient) return <div>Something went wrong.</div>;
+
     return (
-        <div>
-            {patient && (
-                <>
-                    <h1>{patient.firstname + " " + patient.lastname}</h1>
-                </>
-            )}
-        </div>
+        <Table celled striped collapsing>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell colSpan="2">
+                        {patient.firstname} {patient.lastname}
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell>Gender</Table.Cell>
+                    <Table.Cell>{patient.gender_identity}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell>Sex At Birth</Table.Cell>
+                    <Table.Cell>{patient.sex_at_birth}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell>Date of Birth</Table.Cell>
+                    <Table.Cell>{`${
+                        patient.birthdate.getUTCMonth() + 1
+                    }/${patient.birthdate.getUTCDate()}/${patient.birthdate.getUTCFullYear()}`}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell>Age</Table.Cell>
+                    <Table.Cell>{utcToAge(patient.birthdate)}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell>Height (cm)</Table.Cell>
+                    <Table.Cell>{patient.height}</Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>
     );
 };
 
